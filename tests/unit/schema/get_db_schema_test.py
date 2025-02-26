8a2bd0cf8020022bbea68d38b0e62148942d0f9e
kat-kel <klarinet28@gmail.com>
2025-02-26 11:58:03 +0100

    test getting db schema from xml

---
+++ tests/unit/schema/get_db_schema_test.py
@@ -0,0 +1,26 @@
+import unittest
+
+from heurist.cli.schema import get_database_schema
+from heurist.api.client import HeuristAPIClient
+from heurist.api.param_manager import APIParamManager
+
+
+class SchemaTest(unittest.TestCase):
+
+    def setUp(self):
+        params = APIParamManager(debugging=True)
+        self.client = HeuristAPIClient(**params.kwargs)
+        self.debugging = True
+
+    def test(self):
+        db = get_database_schema(
+            record_groups=["My record types"],
+            client=self.client,
+            debugging=True,
+        )
+        actual = len(db.pydantic_models)
+        self.assertGreater(actual, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
