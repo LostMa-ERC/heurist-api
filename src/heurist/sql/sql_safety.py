db6c40aac15202b4a7509a9565be6617ab14f61a
Virgile Reignier <64497404+virgile-reignier@users.noreply.github.com>
2026-04-08 14:26:29 +0200

    add a default value for column fielf_type on SafeSQLName

--- src/heurist/sql/sql_safety.py
+++ src/heurist/sql/sql_safety.py
@@ -56,7 +56,7 @@ class SafeSQLName:
         capitalized_words = ["".join(w[0].capitalize() + w[1:] for w in words)]
         return "".join(capitalized_words)
 
-    def create_column_name(self, field_name: str, field_type: str) -> str:
+    def create_column_name(self, field_name: str, field_type: str = None) -> str:
         """
         Create an SQL-safe column name for the Pydantic data field.
 
